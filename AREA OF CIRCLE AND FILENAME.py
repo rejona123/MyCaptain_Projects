a2529e067e8b82710baadd547888a542f0bfb85c
Rejona Susan. J <93517902+Rejona7023@users.noreply.github.com>
2021-12-22 16:39:24 +0530

    Update and rename TASK_1.py to AREA OF CIRCLE AND FILENAME.py

---
+++ AREA OF CIRCLE AND FILENAME.py
@@ -0,0 +1,16 @@
+# AREA OF CIRCLE
+
+r = float(input('Enter the radius of the cirle: '))
+
+A = 3.14 * r**2
+
+print(A)
+
+#EXTENTION OF FILE NAME
+
+filename = input("Input the Filename: ")
+
+extention = filename.split(".")
+
+print (f'The extension of the file is : .{extention[1]}')    
+
